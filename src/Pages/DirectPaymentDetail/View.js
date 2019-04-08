85b9190dcf1cc311115e5046b028b96db135e5a3
dandan <il>
2019-04-08 22:21:19 +0800

    删除多余注释代码

--- src/Pages/DirectPaymentDetail/View.js
+++ src/Pages/DirectPaymentDetail/View.js
@@ -5,11 +5,9 @@ import {View as StageTextIndicator} from '../../Components/StageTextIndicator';
 import {DIRECT_PAYMENT_STAGE_ID, DIRECT_PAYMENT_STAGE_ID_TO_TEXT} from '../../Constant';
 import {browserHistory, withRouter} from 'react-router';
 import {PAGE_ID_TO_ROUTE, REQUIRE_LOGIN_PAGE_ID} from '../../Config';
-// import {View as InsuranceCompanyVerifyProcessor} from './Components/InsuranceCompanyVerifyProcessor';
 import {getDirectPaymentInfoAction} from './Actions/Actions';
 import {connect} from 'react-redux';
 import NAMESPACE from '../../NAMESPACE';
-// import {View as PayConfirmProcessor} from './Components/PayConfirmProcessor';
 
 class DirectPaymentDetail extends React.Component
 {
@@ -31,12 +29,7 @@ class DirectPaymentDetail extends React.Component
     render()
     {
         const stageTextArray = [...DIRECT_PAYMENT_STAGE_ID_TO_TEXT];
-        // const {insurancePurchasingInfo} = this.props;
         const {directPaymentInfo} = this.props;
-        /*const {
-            [NAMESPACE.INSURANCE_PURCHASING_PROCESS.INSURANCE_PURCHASING_INFO.INSURANCE_PURCHASING_INFO_ID]: insurancePurchasingInfoId,
-            [NAMESPACE.INSURANCE_PURCHASING_PROCESS.INSURANCE_PURCHASING_INFO.INSURANCE_PURCHASING_STAGE]: stageNumber,
-        } = insurancePurchasingInfo;*/
         const {
             [NAMESPACE.DIRECT_PAYMENT_PROCESS.DIRECT_PAYMENT_INFO.DIRECT_PAYMENT_INFO_ID]: directPaymentInfoId,
             [NAMESPACE.DIRECT_PAYMENT_PROCESS.DIRECT_PAYMENT_INFO.DIRECT_PAYMENT_STAGE]: stageNumber,
@@ -52,28 +45,6 @@ class DirectPaymentDetail extends React.Component
                     <StageTextIndicator currentStageNumber={stageNumber}
                                         stageTextArray={stageTextArray} />
                 </div>
-                {/* <div className={Style.stageProcessorWrapper}>
-                    {
-                        (() =>
-                        {
-                            switch (stageNumber)
-                            {
-                                case INSURANCE_PURCHASING_STAGE_ID.INSURANCE_COMPANY_VERIFY:
-                                {
-                                    return <InsuranceCompanyVerifyProcessor insurancePurchasingInfoId={insurancePurchasingInfoId} />;
-                                }
-                                case INSURANCE_PURCHASING_STAGE_ID.PAY:
-                                {
-                                    return <PayConfirmProcessor insurancePurchasingInfo={insurancePurchasingInfo} />;
-                                }
-                                default:
-                                {
-                                    return null;
-                                }
-                            }
-                        })()
-                    }
-                </div> */}
             </div>
         );
     }
@@ -81,7 +52,6 @@ class DirectPaymentDetail extends React.Component
 
 const mapStateToProps = state =>
 {
-    // const {InsurancePurchasingDetail: {insurancePurchasingInfo}} = state;
     const {DirectPaymentDetail: {directPaymentInfo}} = state;
     return {
         directPaymentInfo,
@@ -89,7 +59,6 @@ const mapStateToProps = state =>
 };
 
 const mapDispatchToProps = {
-    //getInsurancePurchasingInfo: getInsurancePurchasingInfoAction,
     getDirectPaymentInfo: getDirectPaymentInfoAction,
 };
 
