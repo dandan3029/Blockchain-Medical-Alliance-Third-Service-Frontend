4c9397ac53716148982c1bb6b2939fb0f59c2083
dandan <il>
2019-04-09 23:58:12 +0800

    添加主页素材路径

---
+++ src/Config/INTRODUCTION_ICON.js
@@ -0,0 +1,9 @@
+import iconSrc1 from '../Static/HomePage/icon1.jpg';
+import iconSrc2 from '../Static/HomePage/icon2.jpg';
+import iconSrc3 from '../Static/HomePage/icon3.jpg';
+
+export default [
+    iconSrc1,
+    iconSrc2,
+    iconSrc3,
+];
